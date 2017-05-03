32d6f6548e1b8e870ac4b499b1bf92f46b5bfd93
Thangavel <thangavel@mails.asahitechnologies.com>
2017-05-03 19:14:56 +0530

    google login

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(function(req,res,next){
-res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
+res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers','authorization');
     next();
 });
