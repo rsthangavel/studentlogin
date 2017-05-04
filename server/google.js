06d3a65d525d91190471878fbe26087096007420
Thangavel <thangavel@mails.asahitechnologies.com>
2017-05-04 09:54:40 +0530

    callback url changed

--- server/google.js
+++ server/google.js
@@ -3,7 +3,7 @@ module.exports = {
     'facebookAuth' : {
         'clientID'      : '469223673272656', // your App ID
         'clientSecret'  : '47bde713789aed4e762368f7108082f1', // your App Secret
-        'callbackURL'   : 'http://localhost:2000/api/auth/facebook/callback'
+        'callbackURL'   : 'https://asdes.herokuapp.com/api/auth/facebook/callback'
     },
 
     'twitterAuth' : {
@@ -15,7 +15,7 @@ module.exports = {
     'googleAuth' : {
         'clientID'      : '681644594140-6u40flstt15k44901so7hvnoh43q7m5n.apps.googleusercontent.com',
         'clientSecret'  : 'OXYwfvElMWfB1It0j9A46Zh6',
-        'callbackURL'   : 'http://localhost:2000/api/auth/google/callback'
+        'callbackURL'   : 'https://asdes.herokuapp.com/api/auth/google/callback'
     }
 
 };
