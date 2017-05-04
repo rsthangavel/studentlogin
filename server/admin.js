954994a5888eb95560630f557d0b4fd2ccebdec4
Thangavel <thangavel@mails.asahitechnologies.com>
2017-05-04 09:49:30 +0530

    admin js file

--- server/admin.js
+++ server/admin.js
@@ -1,12 +1,5 @@
 const mongoose = require('mongoose');
-/*var options = { server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } },
-                replset: { socketOptions: { keepAlive: 300000, connectTimeoutMS : 30000 } } };
- var mongourl = 'mongodb://test:test@ds163940.mlab.com:63940/student';
-mongoose.createConnection(mongourl,options);
-var db = mongoose.connection; 
-db.once("open",function(){
-  console.log("Connection Established");
-}); */
+
 
 var adminLoginSchema = mongoose.Schema({
   adminName : {type:String, required:true},
